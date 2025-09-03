50872ffbc8d19f30d5ef7ea2ebc08f92c0528827
rajababu-tw <raja.babu@thoughtworks.com>
2025-09-03 14:49:47 +0530

    Project Creation

---
+++ src/main/java/com/ford/springboot_integration_demo_session/SpringbootIntegrationDemoSessionApplication.java
@@ -0,0 +1,13 @@
+package com.ford.springboot_integration_demo_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationDemoSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrationDemoSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ford/springboot_integration_demo_session/SpringbootIntegrationDemoSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ford.springboot_integration_demo_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrationDemoSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
